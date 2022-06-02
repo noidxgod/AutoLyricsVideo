19db63700d3143f4f6f50304192e1592a8f766c7
Rodion <anonimys.xxx@mail.ru>
2022-06-03 03:48:09 +0500

    FinallVersion

--- main.py
+++ main.py
@@ -4,16 +4,24 @@ import moviepy.editor as mp
 import numpy as np
 import shutil
 import math
-img1 = cv2.imread('1.png')
-with open('text.txt', 'r') as f:
+import glob
+def findfiles(extension):
+    path = "./*." + str(extension)
+    symbol = ".\ ".replace(" ","")
+    filename = glob.glob(path)[0].replace(symbol,"")
+    return(filename)
+
+first_png_file = findfiles('png')
+img1 = cv2.imread(first_png_file)
+with open(findfiles('txt'), 'r') as f:
     lines = f.read().splitlines()
 for i in range(0,len(lines)):
-    lines[i] = lines[i].replace("ё","е")
+    lines[i] = lines[i].replace("ё","е").upper()
 times = []
 lyrics = []
 x = 0
 for i in range(0,len(lines)):
-    if lines[i] == " ":
+    if lines[i] == " " or lines[i] == "":
         x+=1
     else:
         lines[i] = lines[i].replace("[","")
@@ -21,26 +29,30 @@ for i in range(0,len(lines)):
         times.append(lines[i][0])
         lyrics.append(lines[i][1])
 for i in range(0,x):
-    lines.remove(' ')
+    try:
+        lines.remove(' ')
+    except:
+        lines.remove('')
 for i in range(0,len(times)):
     times[i] = times[i].replace(":",".0:")
     times[i] = times[i].split(":")
     times[i] = math.floor(float(times[i][0])*60 + float(times[i][1]))
-#print(times)
-writer = cv2.VideoWriter("output.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 24,(1920,1080))#MJPG
-print("Введите время в секундах:")
-time = int(input())*24
+nameoutputfile = str(input("Введите имя выходного файла: "))+".mp4"
+writer = cv2.VideoWriter(nameoutputfile, cv2.VideoWriter_fourcc(*"mp4v"), 24,(1920,1080))#MJPG
+time = int(input("Введите время в секундах: "))*24
 times.append(time)
 font = cv2.FONT_HERSHEY_COMPLEX
 j = 0
-
-for frame in range(time):
+font_size = int(input("Введите размер шрифта: "))
+x = int(input("Введите расположение по ширине: "))
+y = int(input("Введите расположение по высоте: "))
+for frame in range(0,time):
     print('\r', 'Процесс', str(frame * 100 // time), '%', end='')
     if (frame==times[j+1]*24):
         j += 1
     if (frame>=(times[j]*24) and frame < (times[j+1]*24)):
-        shutil.copyfile('1.png', 'buffer1.png')
-        shutil.copyfile('1.png', 'buffer2.png')
+        shutil.copyfile(first_png_file, 'buffer1.png')
+        shutil.copyfile(first_png_file, 'buffer2.png')
         try:
             img2 = cv2.imread('buffer1.png')
         except:
@@ -48,19 +60,22 @@ for frame in range(time):
         cv2_im_rgb = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
         pil_im = Image.fromarray(cv2_im_rgb)
         draw = ImageDraw.Draw(pil_im)
-        font = ImageFont.truetype("font1.otf", 24)
-        draw.text((960-len(lyrics[j])*10, 540), lyrics[j], font=font)
+        try:
+            font = findfiles('ttf')
+        except:
+            font = findfiles('otf')
+        font = ImageFont.truetype(font, font_size)
+        draw.text((x, y), lyrics[j], font=font, anchor="ms", align="left")
         cv2_im_processed = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
-        #cv2.putText(img2, lyrics[j], (960-len(lyrics[j])*10, 540), font, 1, color=(255, 255, 255), thickness=2)
         writer.write(cv2_im_processed)
-    else:
+    if (frame<(times[j]*24)):
         writer.write(img1)
 print('\r', str(100), '%', end='')
 writer.release()
 try:
-    audio = mp.AudioFileClip("music.mp3")
-    video1 = mp.VideoFileClip("output.mp4")
+    audio = mp.AudioFileClip(findfiles('mp3'))
+    video1 = mp.VideoFileClip(nameoutputfile)
     final = video1.set_audio(audio)
-    final.write_videofile("output2.mp4")
+    final.write_videofile("result.mp4")
 except:
     print("error")
